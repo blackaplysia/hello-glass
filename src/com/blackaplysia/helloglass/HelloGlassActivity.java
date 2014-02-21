a7f50076bf0c628cbaaabb26a4879e318cace9e3
blackaplysia <blackaplysia@gmail.com>
2014-02-21 18:02:41 +0000

    Set footnote

--- src/com/blackaplysia/helloglass/HelloGlassActivity.java
+++ src/com/blackaplysia/helloglass/HelloGlassActivity.java
@@ -1,18 +1,21 @@
 package com.blackaplysia.helloglass;
 
 import com.google.android.glass.timeline.TimelineManager;
-import com.google.android.glass.widget.CardScrollAdapter;
-import com.google.android.glass.widget.CardScrollView;
 import com.google.android.glass.app.Card;
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.ViewGroup;
-import android.view.View;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class HelloGlassActivity extends Activity
 {
+
+    String createDateString() {
+	Date date = new Date();
+	SimpleDateFormat sdf = new SimpleDateFormat("yyyy'/'MM'/'dd' 'kk':'mm':'ss");
+	return sdf.format(date);
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -22,6 +25,7 @@ public class HelloGlassActivity extends Activity
 	TimelineManager timelineManager = TimelineManager.from(this);
 	Card card1 = new Card(this);
 	card1.setText(R.string.hello_message);
+	card1.setFootnote(createDateString());
 	timelineManager.insert(card1);
 
 	finish();
