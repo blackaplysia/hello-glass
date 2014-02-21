e4e383c65f0c5f5e1175af0f8141442f8027cd42
blackaplysia <blackaplysia@gmail.com>
2014-02-21 17:16:15 +0000

    Insert static card in timeline.

--- src/com/blackaplysia/helloglass/HelloGlassActivity.java
+++ src/com/blackaplysia/helloglass/HelloGlassActivity.java
@@ -1,15 +1,29 @@
 package com.blackaplysia.helloglass;
 
+import com.google.android.glass.timeline.TimelineManager;
+import com.google.android.glass.widget.CardScrollAdapter;
+import com.google.android.glass.widget.CardScrollView;
+import com.google.android.glass.app.Card;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.view.View;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HelloGlassActivity extends Activity
 {
-    /** Called when the activity is first created. */
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
+	TimelineManager timelineManager = TimelineManager.from(this);
+	Card card1 = new Card(this);
+	card1.setText(R.string.hello_message);
+	timelineManager.insert(card1);
+
+	finish();
     }
 }
